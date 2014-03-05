a27475f9a3ce738175cd96441b510dd913514ca1
Adam Lerner <lerner@denovogroup.org>
2014-03-04 16:53:05 -0800

    Send button sends message to 10.0.2.2:51689
    Sending works, can receive strings on nc -ul 51689
    on the development machine.

--- src/org/denovogroup/rangzen/MainActivity.java
+++ src/org/denovogroup/rangzen/MainActivity.java
@@ -1,8 +1,16 @@
 package org.denovogroup.rangzen;
 
 import android.os.Bundle;
+
+import android.util.Log;
+import android.view.View;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +26,21 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void sendMessage(View view) {
+    	EditText editMessage = (EditText) findViewById(R.id.edit_message);
+    	String message = editMessage.getText().toString();
+		Log.i("sendMessage", "sending message "+message);
+
+    	Intent sendIntent = new Intent(this, MessageSendIntentService.class);
+    	sendIntent.putExtra(MessageSendIntentService.MESSAGE_TO_SEND, message);
+    	startService(sendIntent);
+    }
+    
+    private void displayMessage(String message) {
+    	TextView displayMessageView = (TextView) findViewById(R.id.display_message);
+    	displayMessageView.setText(message);
     }
     
 }
