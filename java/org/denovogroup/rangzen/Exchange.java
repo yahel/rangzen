8e30d7f68c7c6fd551b72955d19406a4e7e1ab4b
Adam Lerner <lerner@denovogroup.org>
2015-01-22 19:17:09 -0800

    Limit reads off the network by lengthValueRead to a maximum size.
    Currently set to 10M.
    
    R=barath@denovogroup.org
    BUG=
    
    Review URL: https://de-novo-rietveld.appspot.com/8200001

--- java/org/denovogroup/rangzen/Exchange.java
+++ java/org/denovogroup/rangzen/Exchange.java
@@ -105,6 +105,16 @@ public class Exchange implements Runnable {
   /** Included with Android log messages. */
   private static final String TAG = "Exchange";
 
+  /**
+   * Size, in bytes, of the maximum size message we'll try to read with lengthValueRead.
+   * This is necessary since otherwise a malicious remote party can just provide a huge
+   * size and cause an OutOfMemory error when we allocate a buffer.
+   */
+  private static final int MAX_MESSAGE_SIZE = 10 * MEGABYTES;
+
+  /** Number of bytes in a megabyte. */
+  private static final int MEGABYTES = 1024 * 1024;
+
   /** Synchronized getter for status. */
   /* package */ synchronized Status getExchangeStatus() {
     return mStatus;
@@ -424,6 +434,9 @@ public class Exchange implements Runnable {
                                                       Class<T> messageClass) {
     int length = popLength(inputStream);
     if (length < 0) {
+      return null;
+    } else if (length > MAX_MESSAGE_SIZE) {
+      Log.e(TAG, "Remote party asked us to read " + length + " bytes in a length/value read");
       return null;
     }
     byte[] messageBytes = new byte[length];
