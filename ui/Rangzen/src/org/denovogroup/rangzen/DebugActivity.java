1edf9b6a3d2ff8aaa3eba8cad3ef8174f1679404
Adam Lerner <lerner@denovogroup.org>
2015-05-05 15:09:40 -0700

    Debug page shows list of devices.

--- ui/Rangzen/src/org/denovogroup/rangzen/DebugActivity.java
+++ ui/Rangzen/src/org/denovogroup/rangzen/DebugActivity.java
@@ -8,11 +8,15 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class DebugActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.debug);
+        updateDeviceAreaBox();
         setUpActionBar();
     }
 
@@ -25,6 +29,21 @@ public class DebugActivity extends Activity {
         abTitle.setTextColor(Color.WHITE);
     }
 
+    private void updateDeviceAreaBox() {
+      PeerManager pm = PeerManager.getInstance(this); 
+      List<Peer> peers = pm.getPeers();
+      String peerString = "# of nearby peers: " + peers.size() + "\n";
+      for (Peer p : peers) {
+        peerString += p.toString() + "\n";
+      }
+      TextView peerTextView = (TextView) findViewById(R.id.device_area);
+      if (peerTextView != null) {
+        peerTextView.setText(peerString);
+      } else {
+        Toast.makeText(this, "Couldn't find device_area text box " + R.id.device_area, Toast.LENGTH_SHORT).show();
+      }
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Respond to the action bar's Up/Home button
@@ -34,8 +53,7 @@ public class DebugActivity extends Activity {
             return true;
         }
         if (item.getItemId() == R.id.refresh) {
-            // (TODO) Adam
-            Toast.makeText(this, "Todo Adam", Toast.LENGTH_SHORT).show();
+          updateDeviceAreaBox();
         }
 
         return super.onOptionsItemSelected(item);
